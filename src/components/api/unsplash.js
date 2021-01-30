3f3d672df98165c311129e866cf5d5bd72ba5244
stebbilax <30419537+stebbilax@users.noreply.github.com>
2021-01-30 19:28:53 +0000

    Delete unsplash.js

--- src/components/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 'Client-ID rrxOOm2wxNVnci1QQ8wSkOAzlRe_mlZueQgIq-cModk',
-    }
-});
